234f3f348099fb732e65bb4bf618ee87d1a60bd7
Paola Patella <paolahydra@users.noreply.github.com>
2023-10-27 12:01:37 +0200

    little progress
    need to move back to windows to try GPU processing

--- rescuePCs_PP/extractSnippetsForPCA.m
+++ rescuePCs_PP/extractSnippetsForPCA.m
@@ -1,39 +1,52 @@
-function cProjPC = extractSnippetsForPCA(rez, nPCs, nChannels)
-% extracts principal components for 1D snippets of spikes from all channels
-% loads a subset of batches to find these snippets
-
-% PP: this works, but does not do what I need.
-% I need to use the actual spike timestamps, within each batch (skipping is
-% fine), instead of finding isolate peaks anew (I need to keep a reference
-% to the given cluster. I guess I will have to do this for every cluster,
-% though doing it in parallel would be a lot faster. As long as I don't
-% loose clust ID ????
-
-% isolated_peaks_new finds all the peaks, and for each of them gives the
-% row (timepoint in batch), column (best channel), mu (probably amplitude?)
-
-% get_SpikeSample gets the wf clip centered (check) around the points
-% specified in row, col. Perhaps it can extract multiple channels at once
-% (dimension of output here is 61,1,nspikes) - check better
-
-%check output again:
-% pc_features = readNPY('D:\paola\sampleForPhy\M1_102721_SpikeData\rez2phy_resave\pc_features.npy');
-% spclu = readNPY('D:\paola\Dropbox\dati\Apr2021\Rita\A333_09032021\spike_clusters.npy');
-% pc_feature_ind = readNPY('D:\paola\sampleForPhy\M1_102721_SpikeData\rez2phy_resave\pc_feature_ind.npy');
-
+% function cProjPC = extractSnippetsForPCA(rez, nPCs, nChannels)
+% % extracts principal components for 1D snippets of spikes from all channels
+% % loads a subset of batches to find these snippets
+% 
+% % PP: this works, but does not do what I need.
+% % I need to use the actual spike timestamps, within each batch (skipping is
+% % fine), instead of finding isolate peaks anew (I need to keep a reference
+% % to the given cluster. I guess I will have to do this for every cluster,
+% % though doing it in parallel would be a lot faster. As long as I don't
+% % loose clust ID ????
+% 
+% % isolated_peaks_new finds all the peaks, and for each of them gives the
+% % row (timepoint in batch), column (best channel), mu (probably amplitude?)
+% 
+% % get_SpikeSample gets the wf clip centered (check) around the points
+% % specified in row, col. Perhaps it can extract multiple channels at once
+% % (dimension of output here is 61,1,nspikes) - check better
+% 
+% %check output again:
+% % pc_features = readNPY('D:\paola\sampleForPhy\M1_102721_SpikeData\rez2phy_resave\pc_features.npy');
+% % spclu = readNPY('D:\paola\Dropbox\dati\Apr2021\Rita\A333_09032021\spike_clusters.npy');
+% % pc_feature_ind = readNPY('D:\paola\sampleForPhy\M1_102721_SpikeData\rez2phy_resave\pc_feature_ind.npy');
+debug = 1
+if debug
+    disp('you are in debug mode')
+    cd('/Users/galileo/Dropbox/Data/M1_102721_SpikeData') 
+    edit clusterSingleBatches.m %this might be useful, in relationship to the call of the next one:
+    edit extractPCbatch2  %this might be useful
+    edit learnAndSolve8b_old %check
+end
 st = readNPY('spike_times.npy');
 clu = readNPY('spike_clusters.npy');  %make sure you in the right folder...
+load('rez.mat')
 % I will simply calculate the best channels after loading the spikes, for
 % each one
 
+rez.ops.fproc = fullfile(pwd, 'temp_wh.dat');
 ops = rez.ops;
 Nbatch      = rez.temp.Nbatch;
-NT  	= ops.NT;
+NT  	= ops.NT; %timpoints within each batch
 batchstart = 0:NT:NT*Nbatch;
+
+nPCs = 3;
+NchanNear = min(ops.Nchan, 2*4+1);
+wPCA    = extractPCfromSnippets(rez, nPCs); % extract PCA waveforms pooled over channels
 
 % extract the PCA projections
 CC = zeros(ops.nt0); % initialize the covariance of single-channel spike waveforms
-fid = fopen(ops.fproc, 'r'); % open the preprocessed data file
+fid = fopen('temp_wh.dat', 'r'); % open the preprocessed data file
 
 for ibatch = 1:100:Nbatch % from every 100th batch
     offset = 2 * ops.Nchan*batchstart(ibatch);
